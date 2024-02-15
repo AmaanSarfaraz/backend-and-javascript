495a97ad23c8151d67acd1aa4f27fca00b57b552
AMAAN SARFARAZ <Sarfarazmohammad369@gmail.com>
2024-02-15 12:26:21 +0530

    register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,78 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res, next) => {
-    res.status(200).json({
-        message: "ok",
+    // get user details from frontend
+    // validation -- not empty
+    // check if user already exists : username and email
+    // check for images, check for avatar
+    // upload them to cloudinary, check avatar uploaded to cloudinary
+    // create user object -create ntry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    //  return response
+
+    const {fullName, username, email, password} = req.body
+    // console.log(username);
+
+    if([fullName, email, password, username].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{email}, {username}]
     })
+    if (existedUser) {
+        throw new ApiError(409, "User already exists")
+    }
+
+    // console.log(req.files);
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+    
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if(!avatar){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken -coverImage"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "something went wrong while registering user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered successfully")
+    )    
+
+
+
 })
 
 export { registerUser }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js"
 
 
-dotenv.config({ path: "./env" });
+dotenv.config({ path: "./.env" });
 
 connectDB()  //it will return a promise
   .then(() => {
